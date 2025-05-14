78f3f96d346c86c17f4d1045d25768e0cf6a076d
Han Frisonsova <hanafrisonsova@gmail.com>
2025-05-14 09:56:06 +0200

    predavani parametru z komponenty do homepage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,23 +1,37 @@
 import { MenuItem } from '../../components/MenuItem';
 import './style.css';
+import { useState } from 'react';
 
 export const HomePage = () => {
+  const [menuOpened, setMenuOpened] = useState(false);
+  const [pageTitle, setPageTitle] = useState('Domů');
+  const handleSelectItem = (page) => {
+    setMenuOpened(false);
+    setPageTitle(page);
+  };
+
+  console.log(menuOpened);
   return (
     <>
       <header>
-        <div className="menu">
-          <button className="menu__btn"></button>
+        <div className={menuOpened ? 'menu' : 'menu menu--closed'}>
+          <button
+            className="menu__btn"
+            onClick={() => {
+              setMenuOpened(!menuOpened);
+            }}
+          ></button>
           <div className="menu__items">
-            <MenuItem text="Domů" />
-            <MenuItem text="Naše nabídka" />
-            <MenuItem text="Náš tým" />
-            <MenuItem text="Blog" />
-            <MenuItem text="Kontakt" />
+            <MenuItem text="Domů" onSelect={handleSelectItem} />
+            <MenuItem text="Naše nabídka" onSelect={handleSelectItem} />
+            <MenuItem text="Náš tým" onSelect={handleSelectItem} />
+            <MenuItem text="Blog" onSelect={handleSelectItem} />
+            <MenuItem text="Kontakt" onSelect={handleSelectItem} />
           </div>
         </div>
       </header>
       <main>
-        <h1>Domů</h1>
+        <h1>{pageTitle}</h1>
       </main>
     </>
   );
